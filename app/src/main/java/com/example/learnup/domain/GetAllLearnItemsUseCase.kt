ff6435317c4cdf269f9cf3725c07475ffcc78995
matraskino <pavel.terpilovsky@gmail.com>
2022-07-24 12:59:42 +0300

    first update

--- app/src/main/java/com/example/learnup/domain/GetAllLearnItemsUseCase.kt
+++ app/src/main/java/com/example/learnup/domain/GetAllLearnItemsUseCase.kt
@@ -1,13 +1,11 @@
 package com.example.learnup.domain
 
 
-import com.example.learnup.data.LearnRepositoryImpl
 import kotlinx.coroutines.flow.Flow
 
-class GetAllLearnItemsUseCase(private val learnRepository: LearnRepositoryImpl) {
-    suspend fun execute():Flow<List<ItemLearn>>{
+class GetAllLearnItemsUseCase(private val learnRepository: LearnRepository) {
+    suspend fun execute(): Flow<List<ItemLearn>> {
 
-        val p = learnRepository.getAllLearnItems()
-        return p
+        return learnRepository.getAllLearnItems()
     }
 }
